9f1ed544b8a058718db71f331fb494a738b809a4
szkj-web-he <xuejie.he@datareachable.com>
2023-03-27 13:57:26 +0800

    Update deepCloneData.ts

--- src/Components/Unit/deepCloneData.ts
+++ src/Components/Unit/deepCloneData.ts
@@ -5,11 +5,6 @@
  * @lastModify xuejie.he 2022-06-14
  */
 
-import * as corejs from "core-js";
-
 export const deepCloneData = <T>(data: T): T => {
-    if (!window.structuredClone) {
-        window.structuredClone = corejs.structuredClone;
-    }
     return window.structuredClone(data);
 };
